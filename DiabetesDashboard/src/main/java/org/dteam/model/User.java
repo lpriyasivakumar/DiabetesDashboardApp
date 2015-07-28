e103850557c40cc79efa85d4857df73501b90cfe
Lakshmipriya Sivakumar <lpriya.sivakumar@gmail.com>
2015-07-27 21:24:09 -0400

    Model Classes

---
+++ DiabetesDashboard/src/main/java/org/dteam/model/User.java
@@ -0,0 +1,33 @@
+package org.dteam.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String userID;
+	private String name;
+		
+	public User() {	}
+	
+	
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
