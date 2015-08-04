efa07f7d600b38454b146138440295df6dd903ec
Damien Williams <kingdeejay@yahoo.com>
2015-08-03 22:17:02 -0400

    committig changes from recipe page

--- DiabetesDashboard/src/main/java/org/dteam/controller/RecipeController.java
+++ DiabetesDashboard/src/main/java/org/dteam/controller/RecipeController.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package org.dteam.controller;
 
 import org.springframework.stereotype.Controller;
@@ -14,3 +15,20 @@ public class RecipeController {
 
 }
 
+=======
+package org.dteam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class RecipeController {
+	@RequestMapping("/recipe")
+	public String viewRecipe() {		
+		return "recipe";
+	}	
+
+}
+>>>>>>> committig changes from recipe page
