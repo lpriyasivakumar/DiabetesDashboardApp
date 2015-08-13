d708c5e764bd766cf3336ca3d2a0124291705d62
Lakshmipriya Sivakumar <lpriya.sivakumar@gmail.com>
2015-08-13 17:32:20 -0400

    Fixed error in A1cDAO

--- DiabetesDashboard/src/main/java/org/dteam/dao/MySQLA1cDAO.java
+++ DiabetesDashboard/src/main/java/org/dteam/dao/MySQLA1cDAO.java
@@ -9,7 +9,7 @@ public class MySQLA1cDAO implements A1cDAO {
 	@Override
 	public int addLabValue(double labValue, String userID) throws SQLException {
 		connectToDB();
-		String sql2 = "Delete from a1c WHERE UserID = " + "'" + userID + "'"+"And LabValue<>"+"'"+labValue+"'";			
+		String sql2 = "Delete from a1c WHERE UserID = " + "'" + userID+"'";			
 		String sql = "Insert into a1c (UserID,LabValue) values("+"'"+userID+"','"+labValue+ "')";
 		statement.addBatch(sql);
 		statement.addBatch(sql2);
